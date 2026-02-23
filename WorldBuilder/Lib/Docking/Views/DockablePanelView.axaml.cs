1b2e49f43c8b836ed15a79a6aeb9492858221232
Vanquish-6 <179872957+Vanquish-6@users.noreply.github.com>
2026-02-22 19:43:43 -0800

    Replace drag-to-dock with dropdown menu on viewport panels

--- WorldBuilder/Lib/Docking/Views/DockablePanelView.axaml.cs
+++ WorldBuilder/Lib/Docking/Views/DockablePanelView.axaml.cs
@@ -12,34 +12,14 @@ namespace WorldBuilder.Lib.Docking {
             if (headerBorder != null) {
                 headerBorder.PointerPressed += Header_PointerPressed;
             }
-
-            var dockDragHandle = this.FindControl<Border>("DockDragHandle");
-            if (dockDragHandle != null) {
-                dockDragHandle.PointerPressed += DockDragHandle_PointerPressed;
-            }
         }
 
         private void Header_PointerPressed(object? sender, PointerPressedEventArgs e) {
             if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed) {
-                // If this is inside a window (floating), drag the window
                 var window = TopLevel.GetTopLevel(this) as Window;
                 if (window != null && window.SystemDecorations == SystemDecorations.BorderOnly) {
                     window.BeginMoveDrag(e);
                 }
-            }
-        }
-
-        private async void DockDragHandle_PointerPressed(object? sender, PointerPressedEventArgs e) {
-            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed) {
-                var dragData = new DataObject();
-
-                // Pass ID instead of object reference to be safe across windows
-                if (DataContext is DockablePanelViewModel vm) {
-                    dragData.Set("DockablePanelId", vm.Id);
-
-                    var result = await DragDrop.DoDragDrop(e, dragData, DragDropEffects.Move);
-                }
-                e.Handled = true; // Prevent bubbling to header move
             }
         }
 
